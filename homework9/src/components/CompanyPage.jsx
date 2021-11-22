9dcfad33c55f0c7c4611e2e9845aa5a8796dbc84
priteshp97 <88158414+priteshp97@users.noreply.github.com>
2021-11-22 15:25:15 +0000

    styling update

--- homework9/src/components/CompanyPage.jsx
+++ homework9/src/components/CompanyPage.jsx
@@ -31,24 +31,27 @@ const CompanyPage = () => {
   };
 
   return (
-    <div className="container">
-      {jobs?.map((job) => (
-        <article className="card">
-          <div className="header">
-            <p>{job.title}</p>
-          </div>
-          <div className="container">
-            <p className="company">{job.company_name}</p>
+    <>
+      <h1 className="company-title">{params.companyname}</h1>
+      <div className="container">
+        {jobs?.map((job) => (
+          <article className="card">
+            <div className="header">
+              <p>{job.title}</p>
+            </div>
+            <div className="container">
+              <p className="company">{job.company_name}</p>
 
-            <p>{job.job_type}</p>
-            <p>{job.candidate_required_location}</p>
-          </div>
-          <a href={job.url} target="_blank">
-            Job Link
-          </a>
-        </article>
-      ))}
-    </div>
+              <p>{job.job_type}</p>
+              <p>{job.candidate_required_location}</p>
+            </div>
+            <a href={job.url} target="_blank">
+              Job Link
+            </a>
+          </article>
+        ))}
+      </div>
+    </>
   );
 };
 
